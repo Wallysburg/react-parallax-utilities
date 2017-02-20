eb02c647b9d3f84b3f6a03d57d2cd38700c3cddd
Charlie Davis <cdavis@accusoft.com>
2017-02-19 21:26:16 -0500

    Whelp, that was easy

--- src/parallax-mouseMove/index.jsx
+++ src/parallax-mouseMove/index.jsx
@@ -6,34 +6,36 @@ export default class ParallaxContainer extends Component {
     children: PropTypes.node,
   };
 
+  getDefaultProps() {
+    return {
+      movementStrength: 15,
+    }
+  }
+
   constructor(props) {
     super(props);
     this.state = {};
-  
-    this.parallaxLayers = this.props.parallaxLayers;
+    this.movementStrength = this.props.movementStrength || 15;
     this.handleMouseMove = this.handleMouseMove.bind(this);
   }
 
+
   handleMouseMove(e) {
-    const event = e || window.event;
-
-    const x = event.clientX - this.parallaxBox.offSetLeft;
-    const y = event.clientY - this.parallaxBox.offSetTop;
-
-  }
-
-  getChildParallaxLayers() {
-    return React.Children.toArray(this.props.children)
-  }
-
-  mouseParallax(element, mouseX, mouseY, speed) {
-
+    const movementStrength = this.movementStrength;
+    const height = movementStrength / window.outerHeight;
+    const width = movementStrength / window.outerWidth;
+    const pageX = e.pageX - (window.outerWidth / 2);
+    const pageY = e.pageY - (window.outerHeight / 2);
+    const newvalueX = (width * (pageX * -1));
+    const newvalueY = (height * (pageY * -1));
+    this.mouseMoveParallaxLayer.style.transform = `translate3d(${newvalueX}px, ${newvalueY}px, 0px)`;
   }
 
   render() {
 
     return (
-      <div ref={ref => { this.parallaxBox = ref; }} onMouseMove={this.handleMouseMove}>
+      <div ref={ref => { this.mouseMoveParallaxLayer = ref; }} onMouseMove={this.handleMouseMove}>
+
       </div>
     );
   }
